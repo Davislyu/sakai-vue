f0c4e30ece87f5cf1b98a9e077e6849849ba57ab
Your Name <your.email@example.com>
2024-08-10 19:50:38 +0300

    tailwindConfig- added screen size for smaller screens

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,7 @@ module.exports = {
     plugins: [require('tailwindcss-primeui')],
     theme: {
         screens: {
-            xs: '420px',
+            xs: '400px',
             sm: '576px',
             md: '768px',
             lg: '992px',
